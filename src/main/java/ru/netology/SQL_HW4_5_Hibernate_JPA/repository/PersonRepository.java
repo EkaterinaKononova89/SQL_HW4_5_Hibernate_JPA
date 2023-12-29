817b23d1932a187b1abcbb39d28ec6b2be62041f
Ekaterina Kononova <ekaterina.panova@mail.ru>
2023-12-29 11:30:49 +0500

    add branch jpa-repository-query

--- src/main/java/ru/netology/SQL_HW4_5_Hibernate_JPA/repository/PersonRepository.java
+++ src/main/java/ru/netology/SQL_HW4_5_Hibernate_JPA/repository/PersonRepository.java
@@ -1,7 +1,10 @@
 package ru.netology.SQL_HW4_5_Hibernate_JPA.repository;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.netology.SQL_HW4_5_Hibernate_JPA.entity.PersonUniqueInfo;
 import ru.netology.SQL_HW4_5_Hibernate_JPA.entity.Persons;
@@ -14,11 +17,14 @@ public interface PersonRepository extends JpaRepository<Persons, PersonUniqueInf
     List<Persons> findByCityOfLiving(String city);
 
     // метод, который принимает возраст (age) и возвращает Entity из БД, которые меньше переданного age и отсортированы по возрастанию
-    List<Persons> findByUniqueInfo_AgeLessThan(int age, Sort sort);
+    @Query("SELECT p FROM Persons p WHERE p.uniqueInfo.age < :age")
+    List<Persons> findByUniqueInfo_AgeLessThan(@Param("age") Integer age, Sort sort);
     //или так:
-    //List<Persons> findByUniqueInfo_AgeLessThanOrderByUniqueInfo_Age(Integer age);
+    //@Query("SELECT p FROM Persons p WHERE p.uniqueInfo.age < :age ORDER BY p.uniqueInfo.age")
+    //List<Persons> findByUniqueInfo_AgeLessThanOrderByUniqueInfo_Age(@Param("age") Integer age);
 
 
     // метод, который принимает имя и фамилию (name и surname) и возвращает Entity из БД, которые соответствуют сочетанию name и surname
-    Optional<Persons> findByUniqueInfo_NameAndUniqueInfo_Surname(String name, String surname);
+    @Query("SELECT p FROM Persons p WHERE p.uniqueInfo.name = :name AND p.uniqueInfo.surname = :surname ")
+    Optional<Persons> findByUniqueInfo_NameAndUniqueInfo_Surname(@Param("name") String name, @Param("surname") String surname);
 }
